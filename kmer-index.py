766ba76f6fee1b24e12ddbcb245d2d05f986a573
averyk22 <avery.kuo@gmail.com>
2024-11-11 22:54:55 -0500

    added benchmarking to script

--- kmer-index.py
+++ kmer-index.py
@@ -9,6 +9,9 @@ import sys
 import collections
 from collections import defaultdict
 from parse_fasta_folder import parse_fasta_files  # Assuming parse_fasta_files is in this module
+# for benchmarking purposes
+import time 
+import tracemalloc
 
 # Builds a kmer index given fasta dict and a k value
 def build_kmer_index(fasta_dict, k_value):
@@ -52,19 +55,34 @@ def find_variants(kmer_index, genome_ids):
 
 
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: python3 kmer-index.py <fasta_folder_path> <k_value> <output_file>")
+    if len(sys.argv) != 3:
+        print("Usage: python3 kmer-index.py <fasta_folder_path> <output_file>")
         sys.exit(1)
 
     folder_path = sys.argv[1]
-    k_value = int(sys.argv[2])
-    output_file = sys.argv[3]
+    k_value = 31 # value from paper we found
+    output_file = sys.argv[2]
 
     fasta_sequences = parse_fasta_files(folder_path)
-
+    tracemalloc.start()
     kmer_index = build_kmer_index(fasta_sequences, k_value)
 
     write_kmer_index_to_file(kmer_index, output_file)
+
+    genome_ids = list(fasta_sequences.keys())
+
+    #benchmarking how long to find variants
+    start = time.time()
+    variant_ct, variants = find_variants(kmer_index, genome_ids)
+    end = time.time()
+    total_time = end - start
+
+    #benchmarking memory
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    print(f"Memory for finding variants: Current={current / 1e6:.2f} MB, Peak={peak / 1e6:.2f} MB")
+    print(f"Time for variant searching: {total_time:.4f} seconds")
+
 
 if __name__ == "__main__":
     main()
