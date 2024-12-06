876c887f3f8eaee79114c367213b25ebcddb61e2
Andrea Cheng <acheng41@jhu.edu>
2024-12-05 23:06:39 -0500

    need to add label to debruijn v langmead

--- debruijn.py
+++ debruijn.py
@@ -2,12 +2,13 @@ from parse_fasta_folder import parse_fasta_files
 from nodeclass import Node
 
 # Get Input Fasta Files for pangenome (PG)
-fasta_dict = parse_fasta_files("./Data/DMPK")
+fasta_dict = parse_fasta_files("./Data/toypangenome")
+# fasta_dict = parse_fasta_files("./Data/DMPK")
 # fasta_dict = parse_fasta_files("./Data/toy_dataset")
 
 
 
-k = 31
+k = 5
 k = k-1
 
 kmer_set = set()
@@ -17,29 +18,33 @@ queue = []
 
 fasta_names = list(fasta_dict.keys())
 
+
 for i, key in enumerate(fasta_names):
-    if i == 1:
-        read = fasta_dict[key] + "$"
-        readlen = len(read)
-        node_idx = 0
-        curr_node = ""
-        prev_node = ""
-        for j in range(0, readlen+1):
-            node_kmer = read[j:j+k]
-            if node_kmer not in kmer_set:
-                kmer_set.add(node_kmer)
-                new_node = Node(node_idx, node_kmer)
-                kmer_dic[node_kmer] = new_node
-                node_idx += 1
+    read = fasta_dict[key] + "$"
+    readlen = len(read)
+    node_idx = 0
+    curr_node = ""
+    prev_node = ""
+    for j in range(readlen + 1 -(k)):
+        node_kmer = read[j:j+k]
+        if node_kmer not in kmer_set:
+            kmer_set.add(node_kmer)
+            new_node = Node(node_idx, node_kmer)
+            kmer_dic[node_kmer] = new_node
+            node_idx += 1
 
-            curr_node = kmer_dic[node_kmer]
-            # Link current node to prev_node
-            if prev_node != "":
-                curr_node.add_child(prev_node)
-                prev_node.add_parent(curr_node)
-            prev_node = curr_node
+        curr_node = kmer_dic[node_kmer]
+        curr_node.add_readid(key)
+        # Link current node to prev_node
+        if prev_node != "":
+            curr_node.add_child(prev_node)
+            prev_node.add_parent(curr_node)
+        prev_node = curr_node
 
+        if "$" in node_kmer:
             print(curr_node.nodelabel)
+
+        # print(curr_node.nodelabel)
 
 
 
@@ -67,7 +72,10 @@ def bfs_write(visited, node, fw):
     while queue:          # Creating loop to visit each node
         m = queue.pop(0)
         for m_child in m.children:
-            fw.write("\tS" + str(m.nodeid) + " -> S" + str(m_child.nodeid) + " [ label = "+m_child.nodelabel+" ];\n")
+            if "$" in m.nodelabel: 
+                fw.write("\t" + str(m.nodelabel[:-1]) + " ->" + str(m_child.nodelabel) + " [ label = "+m.nodelabel[-2]+" ];\n")
+            else:
+                fw.write("\t" + str(m.nodelabel) + " ->" + str(m_child.nodelabel) + " [ label = "+m.nodelabel[-1]+" ];\n")
 
         for child in m.children:
             if child not in visited:
@@ -75,12 +83,36 @@ def bfs_write(visited, node, fw):
                 queue.append(child)
 
 
-outfile = "output.txt"
+outfile = "output.dot"
 
-fw = open(outfile, "a")
+fw = open(outfile, "w")
 fw.write("strict digraph  {\n")
 # bfs(visited, kmer_dic["$"])
 visited = []
-bfs_write(visited, kmer_dic["$"], fw) #function for BFS
+bfs_write(visited, kmer_dic["gat$"], fw) #function for BFS
 fw.write("}\n")
 fw.close()
+
+
+
+# import graphviz
+# g = graphviz.Digraph(comment='DeBruijn graph')
+# for node in iter(kmer_dic.keys()):
+#     g.node(kmer_dic[node].nodelabel,kmer_dic[node].nodelabel )
+# visited = []
+# queueu = []
+# node = kmer_dic["GCGAATAAAAGGCCCTCCATCTGCCCAAA$"]
+# visited.append(node)
+# queue.append(node)
+# while queue:          # Creating loop to visit each node
+#     m = queue.pop(0)
+#     for m_child in m.children:
+#         g.edge(m.nodelabel, m_child.nodelabel)
+#     for child in m.children:
+#         if child not in visited:
+#             visited.append(child)
+#             queue.append(child)
+
+
+
+
--- nodeclass.py
+++ nodeclass.py
@@ -4,15 +4,20 @@ class Node:
         self.nodelabel = nodeLabel
         self.parents = []
         self.children = []
+        self.uniquereads = []
     
     def add_parent(self, parent):
         if parent not in self.parents:
             self.parents.append(parent)
 
-
+    
     def add_child(self, child):
         if child not in self.children:
             self.children.append(child)
+
+    def add_readid(self, readID):
+        if readID not in self.uniquereads:
+            self.uniquereads.append(readID)
 
     def set_nodeid(self, nodeID):
         self.nodeid = nodeID
