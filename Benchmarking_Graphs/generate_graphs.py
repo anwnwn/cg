0903e4f26616fe2bfd6e2a4eb37261faac99164d
Andrea Cheng <acheng41@jhu.edu>
2024-12-08 19:19:33 -0500

    ready to submit

--- Benchmarking_Graphs/generate_graphs.py
+++ Benchmarking_Graphs/generate_graphs.py
@@ -24,17 +24,16 @@ def parse_file(file_path):
 
 # Define the files to be processed and their corresponding labels
 file_paths = [
-    '../K-mer_Index/kmer_output.txt', 
-    '../Suffix_Tree/st_implementation/st_output.txt',  
-    '../Suffix_Tree/st_package_implementation/st_output.txt', 
-    '../Wheeler_Graph/wheeler_output.txt'
-
+    './K-mer_Index/kmer_output.txt', 
+    './Suffix_Tree/st_implementation/st_output.txt',  
+    './Suffix_Tree/st_package_implementation/st_output.txt', 
+    './Wheeler_Graph/wheeler_output.txt'
 ]
 labels = [
     'K-mer Index',  # Custom label for the first file
     'Custom Suffix Tree',  # Custom label for the second file
     'Package Suffix Tree',  # Custom label for the third file
-    'Wheeler Graph',  # Custom label for the fourth file
+    'Wheeler Graph'
 ]
 
 # Plotting setup: Create two subplots (one for log scale and one for non-log scale)
@@ -43,7 +42,6 @@ fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
 # Loop over each file and plot its data on both axes
 for file_path, label in zip(file_paths, labels):
     lengths, times = parse_file(file_path)
-    print(file_path)
     
     # Plot on log scale (base 2) for the first axis (ax1)
     ax1.plot(lengths, times, marker='o', linestyle='-', label=label)
@@ -53,17 +51,19 @@ for file_path, label in zip(file_paths, labels):
 
 # Plot configuration for the log scale plot (ax1)
 ax1.set_xscale('log', base=2)
+ax1.set_yscale('log', base = 10)
 ax1.set_title('Query Time vs Query Length (Log Scale)')
 ax1.set_xlabel('Query Length')
 ax1.set_ylabel('Query Time (milliseconds)')
-ax1.grid(True, which="both", ls="--", linewidth=0.5)
+#ax1.grid(True, which="both", ls="--", linewidth=0.5)
 ax1.legend()
 
 # Plot configuration for the non-log scale plot (ax2)
+ax2.set_yscale('log', base = 10)
 ax2.set_title('Query Time vs Query Length (Linear Scale)')
 ax2.set_xlabel('Query Length')
 ax2.set_ylabel('Query Time (milliseconds)')
-ax2.grid(True, which="both", ls="--", linewidth=0.5)
+#ax2.grid(True, which="both", ls="--", linewidth=0.5)
 ax2.legend()
 
 # Adjust layout for better spacing
